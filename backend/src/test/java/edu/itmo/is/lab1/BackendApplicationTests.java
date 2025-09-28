d8ebb1b42ee48f3b8e6880b75b6c0901fb527eaf
Kostya666 <eliseevkonstantin306@gmail.com>
2025-09-28 11:20:25 +0300

    test(back): remove old test and add new test

---
+++ backend/src/test/java/edu/itmo/is/lab1/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.itmo.is.lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // smoke-test: просто проверяем, что контекст поднимается
+    }
+}
